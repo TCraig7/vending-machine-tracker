d24764619df6b464e01172369094094fd85c77a9
Tara Craig <tara.craig7@gmail.com>
2018-09-07 11:00:45 -0600

    Started user story 3, stopped in middle of adding locations to snack show page.

--- spec/features/user_can_see_a_single_snack_spec.rb
+++ spec/features/user_can_see_a_single_snack_spec.rb
@@ -4,11 +4,14 @@ feature 'When a user visits a vending machine show page' do
   scenario 'user sees snacks associated with a vending machine' do
     owner = Owner.create!(name: "Sam's Snacks")
     dons  = owner.machines.create!(location: "Don's Mixed Drinks")
-    snack = dons.snacks.create!(name: 'Butterfinger', price: 1.50)
+    lous  = owner.machines.create!(location: "Lou's Place")
+    snack = dons.snacks.create!(name: 'Butterfinger', price: 1.00)
 
     visit snack_path(snack)
 
     expect(page).to have_content(snack.name)
-    expect(page).to have_content("Price: $1.50")
+    expect(page).to have_content("Price: $1.00")
+    expect(page).to have_content(dons.location)
+    expect(page).to have_content(lous.location)
   end
 end
