29ebcd86db5566524c5d32df88b30e0f195bcc96
Tara Craig <tara.craig7@gmail.com>
2018-09-07 10:13:02 -0600

    Fix failing tests by migrating. Add snacks migration and test.

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,18 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'user sees snacks associated with a vending machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack1 = dons.snacks.create(name: 'Butterfinger', price: 1.50)
+    snack2 = dons.snacks.create(name: 'Snickers', price: 2.50)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Snack: #{snack1.name}")
+    expect(page).to have_content("Price: #{snack1.price}")
+    expect(page).to have_content("Snack: #{snack2.name}")
+    expect(page).to have_content("Price: #{snack2.price}")
+  end
 end
